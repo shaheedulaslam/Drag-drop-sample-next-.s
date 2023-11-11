75dd1b9f5bb3b570cfe280a8dbe7a343ae7052c2
shaheedulaslam <shaheedulaslam061@gmail.com>
2023-11-11 10:13:03 +0530

    drop zone finished

--- src/components/DragDrop.js
+++ src/components/DragDrop.js
@@ -1,66 +1,114 @@
+// import React, { useRef, useState } from 'react';
+// import DragAndDropExample from './DragDrop1';
+
+// export default function DragDrop() {
+//   const dragItem = useRef();
+//   const dragOverItem = useRef();
+//   const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']);
+
+//   const dragStart = (e, position) => {
+//     dragItem.current = position;
+//     e.target.style.cursor = 'grabbing';
+//   };
+
+//   const dragEnter = (e, position) => {
+//     dragOverItem.current = position;
+//     e.target.style.cursor = 'move'; 
+//   };
+
+//   const dragEnd = (e) => {
+//     e.target.style.cursor = 'pointer'; 
+//   };
+
+//   const drop = () => {
+//     const copyListItems = [...list];
+//     const dragItemContent = copyListItems[dragItem.current];
+//     copyListItems.splice(dragItem.current, 1);
+//     copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+//     dragItem.current = null;
+//     dragOverItem.current = null;
+//     setList(copyListItems);
+//   };
+
+//   return (
+//     <>
+//       {list &&
+//         list.map((item, index) => (
+//           <>
+//           <div
+//             style={{
+//               backgroundColor: 'lightblue',
+//               margin: '20px 25%',
+//               textAlign: 'center',
+//               fontSize: '40px',
+//               cursor: 'grab', 
+//             }}
+//             onDragStart={(e) => dragStart(e, index)}
+//             onDragEnter={(e) => dragEnter(e, index)}
+//             onDragEnd={dragEnd}
+//             onDragLeave={dragEnd}
+//             onDragOver={(e) => e.preventDefault()} 
+//             onDrop={drop}
+//             key={index}
+//             draggable>
+//             {item}
+//           </div>
+//           </>
+//         ))}
+//        <DragAndDropExample drop={drop}/>
+//     </>
+//   );
+// }
+
+
+
+
+
 import React, { useRef, useState } from 'react';
 import DragAndDropExample from './DragDrop1';
 
 export default function DragDrop() {
-  const dragItem = useRef();
   const dragOverItem = useRef();
   const [list, setList] = useState(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6']);
+  const [droppedItems, setDroppedItems] = useState([]);
 
-  const dragStart = (e, position) => {
-    dragItem.current = position;
-    e.target.style.cursor = 'grabbing';
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData('text/plain', index);
   };
-
+  
   const dragEnter = (e, position) => {
     dragOverItem.current = position;
     e.target.style.cursor = 'move'; 
   };
 
-  const dragEnd = (e) => {
-    e.target.style.cursor = 'pointer'; 
+  const handleDrop = (index) => {
+    const selectedItem = list[index];
+    const updatedList = list.filter((_, i) => i !== index);
+    setList(updatedList);
+    setDroppedItems([...droppedItems, selectedItem]);
   };
 
-  const drop = () => {
-    const copyListItems = [...list];
-    const dragItemContent = copyListItems[dragItem.current];
-    copyListItems.splice(dragItem.current, 1);
-    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
-    dragItem.current = null;
-    dragOverItem.current = null;
-    setList(copyListItems);
+  const handleDeleteItem = (index) => {
+    const updatedDroppedItems = droppedItems.filter((_, i) => i !== index);
+    setDroppedItems(updatedDroppedItems);
   };
 
   return (
     <>
-      {list &&
-        list.map((item, index) => (
-          <>
+      <div className="flex justify-center flex-wrap">
+        {list.map((item, index) => (
           <div
-            style={{
-              backgroundColor: 'lightblue',
-              margin: '20px 25%',
-              textAlign: 'center',
-              fontSize: '40px',
-              cursor: 'grab', 
-            }}
-            onDragStart={(e) => dragStart(e, index)}
-            onDragEnter={(e) => dragEnter(e, index)}
-            onDragEnd={dragEnd}
-            onDragLeave={dragEnd}
-            onDragOver={(e) => e.preventDefault()} 
-            onDrop={drop}
             key={index}
-            draggable>
+            onDragStart={(e) => handleDragStart(e, index)}
+            onDragEnd={(e)=> dragEnter(e,index)}
+            draggable
+            className="m-2 p-4 bg-blue-200 cursor-move"
+          >
             {item}
           </div>
-          </>
         ))}
-       <DragAndDropExample drop={drop} allowDrop={(e) => e.preventDefault()}/>
+      </div>
+      <DragAndDropExample droppedItems={droppedItems} onDrop={handleDrop} onDeleteItem={handleDeleteItem}/>
     </>
   );
 }
-
-
-
-
-
--- src/components/DragDrop1.js
+++ src/components/DragDrop1.js
@@ -1,44 +1,37 @@
-import React from 'react';
+import React from "react";
 
-
-const DragAndDropExample = ({drop,allowDrop}) => {
+const DragAndDropExample = ({ onDrop, droppedItems,onDeleteItem }) => {
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const index = e.dataTransfer.getData("text/plain");
+    onDrop(index);
+  };
   return (
     <div className="text-center">
-            <div 
-            id="div1"
-            class="p-4 mb-4 rounded bg-gray-50">
-
-            <div class="grid grid-cols-2 gap-4">
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800" onDrop={drop} onDragOver={allowDrop}>
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                </svg>
-              </p>
-            </div>
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800" onDrop={drop} onDragOver={allowDrop}>
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                </svg>
-              </p>
-            </div>
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800" onDrop={drop} onDragOver={allowDrop}>
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                </svg>
-              </p>
-            </div>
-            <div class="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800" onDrop={drop} onDragOver={allowDrop}>
-              <p class="text-2xl text-gray-400 dark:text-gray-500">
-                <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
-                </svg>
-              </p>
-            </div>
-          </div>
-          </div>
+      <div
+        id="div1"
+        onDrop={handleDrop}
+        onDragOver={(e) => e.preventDefault()}
+        class="py-8 mb-4 rounded bg-gray-50"
+      >
+        {droppedItems.length === 0 ?
+          <div className="flex items-center justify-center text-black">
+          <p className="text-center">+</p>
+         </div>:''}
+        <div className="grid grid-cols-2 gap-4">
+          {droppedItems.map((item, index) => (
+            <>
+            <div
+                  key={index}
+                  className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800"
+              >
+                         <button onClick={() => onDeleteItem(index)} className="">delete</button>
+                  {item}
+              </div>
+              </>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
